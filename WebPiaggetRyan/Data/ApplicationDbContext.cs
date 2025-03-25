3777b102efa3a76a9953bcd648193c028101d189
RyanGBC <ryangabrj@gmail.com>
2025-03-25 11:18:34 -0300

    Controles e migration database

--- WebPiaggetRyan/Data/ApplicationDbContext.cs
+++ WebPiaggetRyan/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebPiaggetRyan.Models;
 
 namespace WebPiaggetRyan.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<WebPiaggetRyan.Models.Aluno> Aluno { get; set; } = default!;
 }
--- WebPiaggetRyan/Data/Migrations/20250325141330_Claket.Designer.cs
+++ WebPiaggetRyan/Data/Migrations/20250325141330_Claket.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebPiaggetRyan.Data;
 
@@ -11,9 +12,11 @@ using WebPiaggetRyan.Data;
 namespace WebPiaggetRyan.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325141330_Claket")]
+    partial class Claket
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
